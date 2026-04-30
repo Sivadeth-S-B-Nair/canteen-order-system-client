d11ec90f1fb80786316cbf0ab3ca40ba161e2c63
Salman S <salman@evalogical.com>
2026-04-30 10:25:00 +0530

    Added next js middleware

--- middleware.js
+++ middleware.js
@@ -1,23 +1,57 @@
 import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
 
-const PROTECTED_ROUTES=["/user","/kitchen"]
 const AUTH_ROUTES=["/login","/register"]
+const USER_ROUTES=["/user"]
+const KITCHEN_ROUTES=["/kitchen"]
 
-export function middleware(request){
+export async function middleware(request){
   const {pathname}=request.nextUrl
   const refreshToken=request.cookies.get("refreshToken")?.value
 
-  const isProtected=PROTECTED_ROUTES.some(route=>pathname.startsWith(route))
   const isAuthRoute=AUTH_ROUTES.some(route=>pathname.startsWith(route))
+  const isUserRoute=USER_ROUTES.some(route=>pathname.startsWith(route))
+  const isKitchenRoute=KITCHEN_ROUTES.some(route=>pathname.startsWith(route))
 
-  if(isProtected && !refreshToken){
+  if(!refreshToken && (isKitchenRoute || isUserRoute) ){
     return NextResponse.redirect(new URL("/login",request.url))
   }
   
-  // if(isAuthRoute && refreshToken){
-  //   return NextResponse.redirect(new URL("/login",request.url))
-  // }
-  // return NextResponse.next()
+  let decoded=null
+  if(refreshToken){
+    try{
+      const secret=new TextEncoder().encode(process.env.JWT_REFRESH_SECRET)
+      const {payload}=await jwtVerify(refreshToken,secret)
+      decoded=payload
+    }
+    catch(err){
+      console.error("JWT VERIFY FAILED:", err.message)  
+      const response=NextResponse.redirect(new URL("/login",request.url))
+      response.cookies.delete("refreshToken")
+      return response
+    }
+  }
+
+  if(decoded){
+    const role=decoded.role
+    if(isAuthRoute){
+      if(role==="kitchen"){
+        return NextResponse.redirect(new URL("/kitchen/dashboard",request.url))
+      }
+      else{
+        return NextResponse.redirect(new URL("/user/menu",request.url))
+      }
+    }
+
+    if(isKitchenRoute && role!=="kitchen"){
+      return NextResponse.redirect(new URL("/user/menu",request.url))
+    }
+
+    if(isUserRoute && role!=="user"){
+      return NextResponse.redirect(new URL("/kitchen/dashboard",request.url))
+    }
+  }
+  return NextResponse.next()
 }
 
 export const config={
